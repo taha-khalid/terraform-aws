913acfdfa159a52f48fde7df775f7140a87d5c43
taha-khalid <tahakhalid0320@gmail.com>
2025-06-14 16:23:07 +0500

    Fixed the version constaints issue in versions.tf for aws provider

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.33.0, ~> 5.95.0"
+      version = "~> 5.95.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
